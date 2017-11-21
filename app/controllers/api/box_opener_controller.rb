a808ba97b98d5ab1fc702fc878ca68df41a5ed62
Kazuki Hirata <family.computer59@gmail.com>
2017-11-21 04:38:31 +0000

    Change to use postgre for Heroku server Last

--- app/controllers/api/box_opener_controller.rb
+++ app/controllers/api/box_opener_controller.rb
@@ -5,15 +5,15 @@ class Api::BoxOpenerController < ApplicationController
       @users = User.where("card_no = ?", box_opener_params[:card_no])
       @users.each do |user|
           if user.role.eql?("teacher") then
-              logger.debug user.role + " 5"
-              str = JSON.generate({ "status" => "approval" , "card_no" => box_opener_params[:card_no] })
+              str = JSON.generate({ "status" => "approval" , "rack_no" => "all" })
           else
               # @rentals = Rental.where("status = ? or status = ?", "approval","lending").where("User_id= ? and rental_date <= ? and due_date >= ? ", user.id, Time.parse("00:00"), Time.parse("00:00"))
               @rentals = Rental.where("status = ? or status = ?", "approval","lending")
-              logger.debug user.role + " 5"
               @rentals.each do |rental|
+                # logger.debug user.id + " 5"
+                # logger.debug rental.User_id + " 5"
+                if rental.User_id.eql?(user.id) then
                   str = JSON.generate({ "status" => rental.status, "card_no" => box_opener_params[:card_no], "rack_no" => rental.rack_no })
-                  logger.debug rental.rack_no + " 555"
                   if rental.status.eql?("approval") then
                     rental.status = "lending"
                   else
@@ -27,6 +27,7 @@ class Api::BoxOpenerController < ApplicationController
                     rental.status = "returned"
                   end
                   rental.update(rental_params)
+                end
               end
           end
           render :json => str and return
