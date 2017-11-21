a517533eb462c7910d255f05239bad8e6d941154
Kazuki Hirata <family.computer59@gmail.com>
2017-11-21 03:47:49 +0000

    Change to use postgre for Heroku server TEST

--- app/controllers/api/box_opener_controller.rb
+++ app/controllers/api/box_opener_controller.rb
@@ -5,12 +5,14 @@ class Api::BoxOpenerController < ApplicationController
       @users = User.where("card_no = ?", box_opener_params[:card_no])
       @users.each do |user|
           if user.role.eql?("teacher") then
+              logger.debug user.role + " 5"
               str = JSON.generate({ "status" => "approval" , "card_no" => box_opener_params[:card_no] })
           else
               @rentals = Rental.where("status = ? or status = ?", "approval","lending").where("User_id= ? and rental_date <= ? and due_date >= ? ", user.id, Time.parse("00:00"), Time.parse("00:00"))
-              logger.debug Time.parse("00:00")
+              logger.debug user.role + " 5"
               @rentals.each do |rental|
-                  str = JSON.generate({ "status" => rental.status , "card_no" => box_opener_params[:card_no], "rack_no" => rental.rack_no })
+                  str = JSON.generate({ "status" => rental.status, "card_no" => box_opener_params[:card_no], "rack_no" => rental.rack_no })
+                  logger.debug rental.rack_no + " 555"
                   if rental.status.eql?("approval") then
                     rental.status = "lending"
                   else
